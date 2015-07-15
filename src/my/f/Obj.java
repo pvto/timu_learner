6cc7af63342dd128c01b99702203b7e5c6b86b4e
pvto <paavo.v.toivanen@gmail.com>
2015-07-15 21:13:14 +0300

    add nullSafe<X> helpers

--- src/my/f/Obj.java
+++ src/my/f/Obj.java
@@ -9,4 +9,15 @@ public class Obj {
         for(Object a : A) if (a != null) return a;
         return null;
     }
+    
+    static public boolean nullSafeEquals(Object a, Object b) {
+        if (a == null && b == null) return true;
+        if (a == null) return false;
+        return a.equals(b);
+    }
+    
+    static public int nullSafeHashCode(Object a) {
+        if (a == null) return 0;
+        return a.hashCode();
+    }
 }
